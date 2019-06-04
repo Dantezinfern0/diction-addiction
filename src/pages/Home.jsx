4a56fbea74d6e6741ffaca5981fe63a5ba36450e
dantezinfern0 <danteharasz@gmail.com>
2019-06-04 17:50:16 -0400

    going home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,17 @@ import { Link } from 'react-router-dom'
 export default function MainPage() {
   return (
     <>
-      <h1>
-        <Link to="/dictionary">Dictionary</Link>
-        <p>or</p>
-        <Link to="/thesaurus">Thesaurus</Link>
+      <h1 id="color">
+        Diction-Addiction App
+        <div className="padding">
+          <Link id="color" to="/dictionary">
+            Dictionary
+          </Link>
+          <div id="color">or</div>
+          <Link id="color" to="/thesaurus">
+            Thesaurus
+          </Link>
+        </div>
       </h1>
     </>
   )
