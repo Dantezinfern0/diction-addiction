f1de771be41c0166c30de35c14a4cb23dbd0a0c6
dantezinfern0 <danteharasz@gmail.com>
2019-06-05 16:41:06 -0400

    stuff done if state

--- src/pages/Dictionary.jsx
+++ src/pages/Dictionary.jsx
@@ -2,22 +2,32 @@ import React, { useState } from 'react'
 import Axios from 'axios'
 import HomeButton from '../components/HomeButton.jsx'
 
+
 export default function Dictionary() {
   const apiUrl =
     'https://www.dictionaryapi.com/api/v3/references/collegiate/json/'
   const dicKey = '?key=8b979027-815e-4cca-93ca-e396a7bb4be7'
   const [term, setTerm] = useState('')
   const [info, setInfo] = useState('')
+  const [suggestion, setSuggestion] = useState('')
+  const errorMessage = () => {
+    return console.log('stuff broke')
+  }
   const apiCall = e => {
     e.preventDefault()
     Axios.get(`${apiUrl}${term}${dicKey}`).then(resp => {
-      console.log(resp.data)
-      const data = resp.data[0]
+      console.log('first catch', resp.data[0])
+      if (term.includes(' ') === false){
+      const dataLab = resp.data[0]
       setInfo({
-        def: data.shortdef,
-        type: data.fl,
-        offense: data.meta.offensive
-      })
+        def: dataLab.shortdef,
+        type: dataLab.fl,
+        offense: dataLab.meta.offensive
+      })} else if (term.includes(' ') === true) {
+      setSuggestion({
+        words: resp.data
+      })} else {errorMessage()}
+      console.log('second catch', suggestion.words)
     })
   }
   return (
@@ -33,15 +43,22 @@ export default function Dictionary() {
         />
         <button onClick={apiCall}>Look it Up!</button>
       </form>
-      <h3 className='centered'>Offensive word?</h3>
-      <h3 className={info.offense ? 'red' : 'green'}>{info.offense ? 'Totally Offensive: use with extreme caution' : 'Good to Go'}</h3>
-      <h3>Word: {term}</h3>
-      <h3>Type: {info.type}</h3>
-      <ol>
-        {info.def && info.def.map((item, i) => {
-          return <li key={i}>{item}</li>
-        })}
-      </ol>
+      <div className="squeeze">
+        <h3 className="centered">Offensive word?</h3>
+        <h3 className={info.offense ? 'red' : 'green'}>
+          {info.offense
+            ? 'Totally Offensive: use with extreme caution'
+            : 'Good to Go'}
+        </h3>
+        <h3>Word: {term}</h3>
+        <h3>Type: {info.type}</h3>
+        <ol>
+          {info.def &&
+            info.def.map((item, i) => {
+              return <li key={i}>{item}</li>
+            })}
+        </ol>
+      </div>
       <HomeButton />
     </>
   )
--- src/pages/Thesaurus.jsx
+++ src/pages/Thesaurus.jsx
@@ -15,8 +15,10 @@ export default function Thesaurus() {
       const data = resp.data[0]
       setDefinition({
         definition: data.shortdef[0],
+        type: data.fl,
         syn: data.meta.syns[0],
-        off: data.meta.offensive
+        off: data.meta.offensive,
+        ants: data.meta.ants[0]
       })
     })
   }
@@ -34,16 +36,33 @@ export default function Thesaurus() {
         />
         <button onClick={apiCaller}>Look it Up!</button>
       </form>
-      <h3 className='centered'>Offensive word?</h3>
-      <h3 className={definition.off ? 'red' : 'green'}>{definition.off ? 'Totally Offensive: use with extreme caution' : 'Good to Go'}</h3>
-      <h3>Word: {word}</h3>
-      <h3>definition: {definition.definition}</h3>
-      <ol><h3>Synonyms</h3>
-        {definition.syn &&
-          definition.syn.map((item, i) => {
-            return <li key={i}>{item}</li>
-          })}
-      </ol>
+      <div className="squeeze">
+        <h3 className="centered">Offensive word?</h3>
+        <h3 className={definition.off ? 'red' : 'green'}>
+          {definition.off
+            ? 'Totally Offensive: use with extreme caution'
+            : 'Good to Go'}
+        </h3>
+        <h3>Word: {word}</h3>
+        <h3>Type: {definition.type}</h3>
+        <h3>Definition: {definition.definition}</h3>
+        <div className="flex">
+          <ol>
+            <h3>Synonyms</h3>
+            {definition.syn &&
+              definition.syn.map((item, i) => {
+                return <li key={i}>{item}</li>
+              })}
+          </ol>
+          <ol>
+            <h3>Antonyms</h3>
+            {definition.ants &&
+              definition.ants.map((item, i) => {
+                return <li key={i}>{item}</li>
+              })}
+          </ol>
+        </div>
+      </div>
       <HomeButton />
     </>
   )
