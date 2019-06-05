434d194421a958ff5521d4aa95a2a1a92c94ba6e
dantezinfern0 <danteharasz@gmail.com>
2019-06-05 14:33:01 -0400

    fixed stuff thanks mark

--- src/pages/Dictionary.jsx
+++ src/pages/Dictionary.jsx
@@ -1,8 +1,15 @@
 import React, { useState } from 'react'
 import Axios from 'axios'
 import HomeButton from '../components/HomeButton.jsx'
+import ListItem from '../components/ListItem.jsx'
 
 export default function Dictionary() {
+  // let def = []
+  // const mapIt = () => {
+  //   for (let i = 0; i < def.length; i++) {
+  //     return <li>{def[i]}</li>
+  //   }
+  // }
   const apiUrl =
     'https://www.dictionaryapi.com/api/v3/references/collegiate/json/'
   const dicKey = '?key=8b979027-815e-4cca-93ca-e396a7bb4be7'
@@ -14,7 +21,13 @@ export default function Dictionary() {
       setTerm({
         name: resp.data[0].hwi.hw,
         type: resp.data[0].fl,
-        def: resp.data[0].shortdef
+        def: resp.data[0].shortdef,
+        def2: resp.data[0].shortdef[1],
+        def3: resp.data[0].shortdef[2],
+        def4: resp.data[0].shortdef[3],
+        def5: resp.data[0].shortdef[4],
+        def6: resp.data[0].shortdef[5],
+        def7: resp.data[0].shortdef[6]
       })
     })
   }
@@ -31,8 +44,19 @@ export default function Dictionary() {
         />
         <button onClick={apiCall}>Look it Up!</button>
       </form>
-      <p>Word: {term.name}</p>
-      <p>Type: {term.type}</p>
+        <h6>Word: {term.name}</h6>
+        <h6>Type: {term.type}</h6>
+      <ol>
+        { term.def && <li>Def: {term.def}</li>}
+        { term.def2 && <li>Def: {term.de2}</li>}
+        { term.def3 && <li>Def: {term.def3}</li>}
+        { term.def4 && <li>Def: {term.def4}</li>}
+        { term.def5 && <li>Def: {term.def5}</li>}
+        { term.def6 && <li>Def: {term.def6}</li>}
+        { term.def7 && <li>Def: {term.def7}</li>}
+        <ListItem/>
+      </ol>
+      {/* {mapIt} */}
       {/* <ol>{term.def.map(donkey => {
         return <li>{donkey}</li>
       })}</ol> */}
--- src/pages/Thesaurus.jsx
+++ src/pages/Thesaurus.jsx
@@ -1,23 +1,27 @@
 import React, { useState } from 'react'
 import Axios from 'axios'
 import HomeButton from '../components/HomeButton.jsx'
+import ListItem from '../components/ListItem.jsx'
 
 export default function Thesaurus() {
   const apiUrl =
     'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/'
   const theKey = '?key=8c234c39-cdc9-45fb-93b7-86ebbfd16948'
   const [word, setWord] = useState('')
+  const [definition, setDefinition] = useState({})
   const apiCaller = e => {
     e.preventDefault()
     Axios.get(`${apiUrl}${word}${theKey}`).then(resp => {
       console.log(resp.data)
-      setWord({
-        name: resp.data[0].hwi.hw,
-        type: resp.data[0].fl,
-        syn: resp.data[0].meta.syns
+      const data = resp.data[0]
+      setDefinition({
+        definition: data.shortdef[0],
+        word: word,
+        syn: data.meta.syns[0]
       })
     })
   }
+  console.log({ definition })
   return (
     <>
       <h1>Thesaurus</h1>
@@ -31,11 +35,20 @@ export default function Thesaurus() {
         />
         <button onClick={apiCaller}>Look it Up!</button>
       </form>
-      <p>Word: {word.name}</p>
-      <p>Type: {word.type}</p>
-      {/* <ol>{word.syn.map(donkey => {
-        return <li>{donkey}</li>
-      })}</ol> */}
+      <p>Word: {definition.word}</p>
+      <p>definition: {definition.definition}</p>
+      <ol>
+        {definition.syn &&
+          definition.syn.map(item => {
+            return <li>{item}</li>
+          })}
+
+        {/* {word.syn && <li>Type: {word.syn}</li>}
+        {word.syn2 && <li>Type: {word.syn2}</li>}
+        {word.syn3 && <li>Type: {word.syn3}</li>}
+        {word.syn4 && <li>Type: {word.syn4}</li>} */}
+        <ListItem />
+      </ol>
       <HomeButton />
     </>
   )
