741935fd83b9ef23a862b2bef0372a3f430293af
dantezinfern0 <danteharasz@gmail.com>
2019-06-04 20:05:32 -0400

    mvp

--- src/pages/Dictionary.jsx
+++ src/pages/Dictionary.jsx
@@ -2,35 +2,39 @@ import React, { useState } from 'react'
 import Axios from 'axios'
 import HomeButton from '../components/HomeButton.jsx'
 
-
 export default function Dictionary() {
   const apiUrl =
     'https://www.dictionaryapi.com/api/v3/references/collegiate/json/'
   const dicKey = '?key=8b979027-815e-4cca-93ca-e396a7bb4be7'
   const [term, setTerm] = useState('')
-  const apiCall = (e) => {
+  const apiCall = e => {
     e.preventDefault()
     Axios.post(`${apiUrl}${term}${dicKey}`).then(resp => {
       console.log(resp.data)
-      setTerm(resp.data)
+      setTerm({
+        name: resp.data[0].hwi.hw,
+        type: resp.data[0].fl,
+        def: resp.data[0].shortdef
+      })
     })
   }
-    return (
-      <>
+  return (
+    <>
       <h1>Dictionary</h1>
-        <form onSubmit={apiCall}>
-          <input
-            type="text"
-            placeholder="Words Words Words"
-            value={term}
-            onChange={e => {
-              setTerm(e.target.value)
-            }}
-          />
-          <button onClick={apiCall}>Look it Up!</button>
-        </form>
-        <HomeButton/>
-      </>
-    )
-  }
-
+      <form onSubmit={apiCall}>
+        <input
+          type="text"
+          placeholder="Enter Word"
+          onChange={e => {
+            setTerm(e.target.value)
+          }}
+        />
+        <button onClick={apiCall}>Look it Up!</button>
+      </form>
+      <p>Word: {term.name}</p>
+      <p>Type: {term.type}</p>
+      <p>Definition: {term.def}</p>
+      <HomeButton />
+    </>
+  )
+}
--- src/pages/Thesaurus.jsx
+++ src/pages/Thesaurus.jsx
@@ -2,34 +2,40 @@ import React, { useState } from 'react'
 import Axios from 'axios'
 import HomeButton from '../components/HomeButton.jsx'
 
-
 export default function Thesaurus() {
-  const apiUrl = 'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/'
+  const apiUrl =
+    'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/'
   const theKey = '?key=8c234c39-cdc9-45fb-93b7-86ebbfd16948'
   const [word, setWord] = useState('')
-  const apiCaller = (e) => {
+  const apiCaller = e => {
     e.preventDefault()
     Axios.post(`${apiUrl}${word}${theKey}`).then(resp => {
       console.log(resp.data)
-      setWord(resp.data)
+      setWord({
+        name: resp.data[0].hwi.hw,
+        type: resp.data[0].fl,
+        def: resp.data[0].shortdef
+      })
     })
   }
-    return (
-      <>
+  return (
+    <>
       <h1>Thesaurus</h1>
-        <form onSubmit={apiCaller}>
-          <input
-            type="text"
-            placeholder="Words Words Words"
-            value={word}
-            onChange={e => {
-              setWord(e.target.value)
-            }}
-          />
-          <button onClick={apiCaller}>Look it Up!</button>
-        </form>
-        <HomeButton/>
-      </>
-    )
-  }
-
+      <form onSubmit={apiCaller}>
+        <input
+          type="text"
+          placeholder="Enter Word"
+          onChange={e => {
+            setWord(e.target.value)
+          }}
+        />
+        <button onClick={apiCaller}>Look it Up!</button>
+      </form>
+      <h3>Word: {word.name}</h3>
+      <h3>Type: {word.type}</h3>
+      <h3>Definition: {word.def}</h3>
+      
+      <HomeButton />
+    </>
+  )
+}
